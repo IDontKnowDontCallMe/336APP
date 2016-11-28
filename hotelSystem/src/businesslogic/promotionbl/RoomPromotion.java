b949a71d89a9ee09fe5e750b5b122c3ee320ed86
samperson1997 <2474809464@qq.com>
2016-11-29 00:31:51 +0800

    2016/11/29 00:31 szs 修改promotionbl的一些框架

--- hotelSystem/src/businesslogic/promotionbl/RoomPromotion.java
+++ hotelSystem/src/businesslogic/promotionbl/RoomPromotion.java
@@ -1,14 +1,32 @@
 package businesslogic.promotionbl;
 
-import po.CustomerPO;
-import po.OrderPO;
+
+import vo.CustomerVO;
+import vo.HotelPromotionVO;
+import vo.OrderVO;
+import java.util.List;
+import data.promotiondata.HotelPromotionDaoImpl;
 
 public class RoomPromotion implements PromotionType{
 
+	private HotelPromotionDaoImpl dao;
+	private List<HotelPromotionVO> hotelPromotionList;
+	private double discount;
+	
+
 	@Override
-	public double getDiscount(OrderPO opo, CustomerPO cpo) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getDiscount(OrderVO ovo, CustomerVO cvo) {
+
+		dao = new HotelPromotionDaoImpl();
+//		hotelPromotionList = dao.getHotelPromotionObject(opo.getHotelID());
+		discount=1.0;
+		
+		for(HotelPromotionVO promotion: hotelPromotionList){
+			if(promotion.equals("")){
+				discount=1.0;
+			}
+		}
+		return discount;
 	}
 
 }
