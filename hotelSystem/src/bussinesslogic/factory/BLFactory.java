26a855f73f2a688f627ff4313dbe2e7f183ddc5d
TXin96 <609012488@qq.com>
2016-11-28 14:35:26 +0800

    BLFactory修改

--- hotelSystem/src/bussinesslogic/factory/BLFactory.java
+++ hotelSystem/src/bussinesslogic/factory/BLFactory.java
@@ -1,20 +1,12 @@
 package bussinesslogic.factory;
 
 import java.rmi.Remote;
-import businesslogicservice.customerblservice.CreditblService;
 import businesslogicservice.customerblservice.CustomerBLService;
-import businesslogicservice.customerblservice.CustomerInfoblService;
-import businesslogicservice.customerblservice.VIPblService;
-import businesslogicservice.hotelblservice.CommentblService;
-import businesslogicservice.hotelblservice.HotelInfoblService;
+import businesslogicservice.hotelblservice.HotelBLService;
 import businesslogicservice.orderblservice.OrderBLService;
 import businesslogicservice.promotionblservice.PromotionBLService;
 import businesslogicservice.roomblservice.RoomBLService;
-import businesslogicservice.userblservice.CustomerblService;
-import businesslogicservice.userblservice.HotelblService;
-import businesslogicservice.userblservice.LoginblService;
 import businesslogicservice.userblservice.UserBLService;
-import businesslogicservice.userblservice.WebMarketerblService;
 
 public class BLFactory {
 	private Remote remote;
@@ -31,28 +23,12 @@ public class BLFactory {
 		this.remote = remote;
 	}
 	
-	public CreditblService getCreditblService() {
-		return (CreditblService)remote;
-	}
-	
 	public CustomerBLService getCustomerBLService() {
 		return (CustomerBLService)remote;
 	}
 	
-	public CustomerInfoblService getCustomerInfoblService() {
-		return (CustomerInfoblService)remote;
-	}
-	
-	public VIPblService getVIPblService() {
-		return (VIPblService)remote;
-	}
-	
-	public CommentblService getCommentblService() {
-		return (CommentblService)remote;
-	}
-	
-	public HotelInfoblService getHotelInfoblService() {
-		return (HotelInfoblService)remote;
+	public HotelBLService getHotelBLService() {
+		return (HotelBLService)remote;
 	}
 	
 	public OrderBLService getOrderBLService() {
@@ -67,24 +43,9 @@ public class BLFactory {
 		return (RoomBLService)remote;
 	}
 	
-	public CustomerblService getCustomerblService() {
-		return (CustomerblService)remote;
-	}
-	
-	public HotelblService getHotelblService() {
-		return (HotelblService)remote;
-	}
-	
-	public LoginblService getLoginblService() {
-		return (LoginblService)remote;
-	}
 	
 	public UserBLService getUserBLService() {
 		return (UserBLService)remote;
 	}
 	
-	public WebMarketerblService getWebMarketerblService() {
-		return (WebMarketerblService)remote;
-	}
-
 }
