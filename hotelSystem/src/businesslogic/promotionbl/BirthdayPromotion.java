f588f724eee06202585c33413fa1e151814734d1
samperson1997 <2474809464@qq.com>
2016-12-01 19:07:31 +0800

    2016/12/01 19:07 szs hotelpromotion的每一个

--- hotelSystem/src/businesslogic/promotionbl/BirthdayPromotion.java
+++ hotelSystem/src/businesslogic/promotionbl/BirthdayPromotion.java
@@ -1,15 +1,35 @@
 package businesslogic.promotionbl;
 
+import java.util.List;
+
 import vo.CalculationConditionVO;
 import vo.CustomerVO;
-import vo.OrderVO;
+import vo.HotelPromotionVO;
 
-public class BirthdayPromotion implements PromotionType{
+public class BirthdayPromotion implements PromotionType {
+
+	HotelPromotionImpl hotelPromotionImpl;
+	double discount;
 
 	@Override
 	public int calculateOrder(CalculationConditionVO calculationVO, CustomerVO customerVO) {
-		// TODO Auto-generated method stub
-		return 0;
+		List<HotelPromotionVO> list = hotelPromotionImpl.getHotelPromotionList(calculationVO.hotelID);
+		discount = 1.0;
+		for (HotelPromotionVO vo : list) {
+			if (vo.promotionType.equals("客户生日促销策略")) {
+				if (customerVO.isBirthVIP && (calculationVO.startDate.isBefore(customerVO.birthday)
+						|| calculationVO.startDate.equals(customerVO.birthday)
+						|| calculationVO.endDate.isAfter(customerVO.birthday))) {
+					discount = vo.discount;
+				}
+			}
+		}
+		int days = (int) (calculationVO.endDate.toEpochDay() - calculationVO.startDate.toEpochDay());
+		int result = (int) (calculationVO.roomNum * calculationVO.roomPrice * discount
+				+ calculationVO.roomNum * calculationVO.roomPrice * discount * (days - 1));
+		System.out.print("按照客户生日促销策略计算，折扣为" + discount + ",总价为" + result);
+
+		return result;
 	}
 
 }
